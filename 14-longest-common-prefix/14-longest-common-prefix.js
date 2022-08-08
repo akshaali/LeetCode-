c177871a55c7b19097195175ba5c80a0155fb113
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-08-08 20:21:01 +0530

    Time: 345 ms (5.09%), Space: 50.1 MB (5.20%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+   if(strs.length === 0 || strs.includes("")){
+       return "";
+   }else if(strs.length ===1){
+       return strs[0];
+   }else{
+        let out = strs[0][0];
+       let index = 1;
+      while(out.length <= strs[0].length){
+          
+           for(let i = 1; i<strs.length; i++){
+            console.log('outttt',out,index, strs[i].slice(0,index)  )
+           if(out !== strs[i].slice(0,index)){
+               console.log('HEyyyyyy', out)
+               return out.slice(0, index-1);
+           }
+       }
+        index +=1;
+        out += strs[0][index-1];
+      }
+       return out.slice(0, index-1);
+   }
+    
+};
