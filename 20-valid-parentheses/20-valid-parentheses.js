3f30d9a780447d160d2a878576519847f7c89a07
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-08-08 22:32:19 +0530

    Time: 92 ms (55.33%), Space: 42.4 MB (49.37%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  if (s.length % 2 !== 0) {
+    return false;
+  } else {
+    const leftSymbol = [];
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === '{' || s[i] === '(' || s[i] === '[') {
+        leftSymbol.push(s[i]);
+      } else if (
+        (s[i] === '}' && leftSymbol.length !== 0 && leftSymbol.slice(-1)[0] === '{') ||
+        (s[i] === ']' && leftSymbol.length !== 0 && leftSymbol.slice(-1)[0] === '[') ||
+        (s[i] === ')' && leftSymbol.length !== 0 && leftSymbol.slice(-1)[0] === '(')
+      ) {
+        leftSymbol.pop();
+      }else{
+          return false;
+      }
+    }
+      console.log(leftSymbol)
+      return !leftSymbol.length;
+  }
+};
