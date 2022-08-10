e299c2d199990a48b4cbade0aab2434100a411af
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-08-10 18:34:56 +0530

    Time: 4 ms (41.31%), Space: 5.9 MB (70.41%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,21 @@
+#include <typeinfo>
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n ==1){
+            return true;
+        }
+        if(n%2 != 0 || n == 0 || n <0){
+            return false;
+        }
+        while(n>2){
+            n = n/2;
+            if(n%2 != 0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
