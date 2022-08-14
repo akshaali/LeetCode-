5443f24251f62624b18c095bb2728f9f8498a533
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-08-14 12:30:25 +0530

    Time: 125 ms (43.08%), Space: 59.7 MB (8.15%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -5,16 +5,15 @@ public:
         int left = 0, right = arr.size() -1, mid;
         mid = (left+right)/2;
         while(left < right){
-           
-            cout<<mid<<endl;
+             // mid = (left+right)/2;
+            cout<<mid<<" "<<left<<" "<<right<<endl;
             if(arr[mid] < arr[mid+1]){
                 left = mid+1;
             }else{
                 right = mid;
             }
-         mid = (left+right)/2;
+           mid = (left+right)/2;
         }
         return mid;
-        
     }
 };
