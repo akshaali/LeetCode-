b5fc83687f4116223ff63dda66a846863486e32d
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-08-30 00:09:36 +0530

    Time: 198 ms (49.49%), Space: 61.3 MB (61.86%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-       int low = 1, high = nums.size() - 1, cnt;
-        while(low <=  high)
-        {
-            int mid = low + (high - low) / 2;
-            cnt = 0;
-            // cnt number less than equal to mid
-            for(int n : nums)
-            {
-                if(n <= mid)
-                    ++cnt;
-            }
-            // binary search on left
-            if(cnt <= mid)
-                low = mid + 1;
-            else
-            // binary search on right
-                high = mid - 1;
+    int findDuplicate(vector<int>& arr) {
+//        int low = 1, high = nums.size() - 1, cnt;
+//         while(low <=  high)
+//         {
+//             int mid = low + (high - low) / 2;
+//             cnt = 0;
+//             // cnt number less than equal to mid
+//             for(int n : nums)
+//             {
+//                 if(n <= mid)
+//                     ++cnt;
+//             }
+//             // binary search on left
+//             if(cnt <= mid)
+//                 low = mid + 1;
+//             else
+//             // binary search on right
+//                 high = mid - 1;
             
-        }
-        return low;
+//         }
+//         return low;
+         for(int i=0;i<arr.size();i++){
+        
+        // finding first negative elemnet
+        if(arr[abs(arr[i])-1]<0)return abs(arr[i]);
+        
+		// if the number is positive make it negative
+        else arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
+                   
     }
+    return 0;
+        
+    }
+       
 };
