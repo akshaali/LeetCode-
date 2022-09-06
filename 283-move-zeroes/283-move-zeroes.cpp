ccc2524cd65b29d881cabc53875435868101bdb9
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-06 16:00:24 +0530

    Time: 54 ms (19.84%), Space: 19.2 MB (83.52%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int zeroPointer=0, nonZeroPointer=0, size = nums.size();
-        int i = 0;
         while(zeroPointer<size && nonZeroPointer <size){
             if(nums[nonZeroPointer] !=0){
                 swap(nums[zeroPointer], nums[nonZeroPointer]);
