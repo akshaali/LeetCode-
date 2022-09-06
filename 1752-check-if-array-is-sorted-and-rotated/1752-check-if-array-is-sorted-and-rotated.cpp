ee808fdd2650de2bb31232c74b265f4a705eea54
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-06 18:08:07 +0530

    Time: 3 ms (66.27%), Space: 8.3 MB (75.60%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -3,24 +3,39 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int k =-1, size = nums.size();
-        for(int i =0; i<size-1; i++){
-            if(nums[i]>nums[i+1]){
-                k = i+1;
-                break;
+        //solution 1
+        // for(int i =0; i<size-1; i++){
+        //     if(nums[i]>nums[i+1]){
+        //         k = i+1;
+        //         break;
+        //     }
+        // }
+        // if(k==-1){
+        //     return true;
+        // }
+        // reverse(nums, 0, k-1);
+        // reverse(nums, k, size-1);
+        // reverse(nums, 0, size-1);
+        // for(int i =0; i<size-1; i++){
+        //     if(nums[i]>nums[i+1]){
+        //        return false;
+        //     }
+        // }
+        // return true;
+        
+        
+        //solution 2 
+        int count = 0;
+        for(int i=1;i<size;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
             }
         }
-        if(k==-1){
-            return true;
+        if(nums[size-1]>nums[0]){
+            count++;
         }
-        reverse(nums, 0, k-1);
-        reverse(nums, k, size-1);
-        reverse(nums, 0, size-1);
-        for(int i =0; i<size-1; i++){
-            if(nums[i]>nums[i+1]){
-               return false;
-            }
-        }
-        return true;
+        return count<=1;
+        
     }
     void reverse(vector<int>& nums, int i, int j){
         while(i<=j){
