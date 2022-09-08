ec28b8e877290084511bb98953854ed634b48c80
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-08 10:13:24 +0530

    Time: 30 ms (11.75%), Space: 10.4 MB (16.76%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,14 +1,13 @@
-/**
- * @param {string[]} sentences
- * @return {number}
- */
-var mostWordsFound = function(sentences) {
-    let max = 0;
-    for(let i = 0; i<sentences.length; i++){
-        let words = sentences[i].split(' ').length;
-        if(words > max){
-            max = words;
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxi = 0;
+        for(auto word: sentences){
+            int spaceCount = count(word.begin(), word.end(), ' ') +1;
+            maxi = max(maxi, spaceCount );
         }
+        return maxi;
+        
+        
     }
-    return max;
 };
