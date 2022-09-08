289d2ce9eaf2d6618e29fe1a2b08b635403d64ea
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-08 08:59:04 +0530

    Time: 17 ms (8.49%), Space: 9.5 MB (96.83%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> out;
-        for(int i = 0; i < n; i++){
-            out.push_back(nums[i]);
-            out.push_back(nums[n+i]);
+        // Time complexity = O(n) space complexity = O(n)
+        // vector<int> out;
+        // for(int i = 0; i < n; i++){
+        //     out.push_back(nums[i]);
+        //     out.push_back(nums[n+i]);
+        // }
+        // return out;
+        
+        //Time complexity = O(n) space complexity = O(1)
+        for(int i = n; i < nums.size(); i++){
+            nums[i] = (nums[i] * 1024) + nums[i-n];
         }
-        return out;
+        int index = 0;
+        for(int i = n; i < nums.size(); i++, index+=2){
+            nums[index] = nums[i]%1024;
+            nums[index+1]= nums[i]/1024;
+        }
+        return nums;
         
     }
 };
