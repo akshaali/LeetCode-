6a99c1380bc25ca47e219eb47af7a86ada0d635f
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-10 12:37:35 +0530

    Time: 4 ms (86.67%), Space: 10.1 MB (8.10%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -1,22 +1,14 @@
-using namespace std;
 class Solution {
 public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int>res;
         for(int i = 0; i<nums.size(); i += 2){
             int num = nums[i];
-            cout<<"num has arrived: "<<num<<"values of i: "<< i<<endl;
-            // for(int j = 0; j<num; j++){
-            //      cout<<num<<endl;
-            // }
             while(num>0){
-                cout<<num<<endl;
                 res.push_back(nums[i+1]);
                 num = num - 1;
             }
         }
         return res;
-        
-        
     }
 };
