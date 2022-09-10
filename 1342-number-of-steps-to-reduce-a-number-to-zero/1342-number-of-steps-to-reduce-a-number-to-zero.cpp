5f465669c4a164795f5add0cb6043ef485ffbaa6
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-10 16:35:14 +0530

    Time: 3 ms (38.54%), Space: 5.9 MB (25.18%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        int count = 0;
-        while(num>0){
-            if(num % 2 == 0){
-                num /= 2;
-            }else {
-                num -= 1;
-            }
-            count++;
+        // int count = 0;
+        // while(num>0){
+        //     if(num % 2 == 0){
+        //         num /= 2;
+        //     }else {
+        //         num -= 1;
+        //     }
+        //     count++;
+        // }
+        // bit manipulation solution
+        if(!num) return 0;
+        int res = 0;
+        while(num) {
+            res += (num & 1) ? 2 : 1;
+            num >>= 1;
         }
-        return count;
+        return res - 1;
+        // return count;
         
     }
 };
