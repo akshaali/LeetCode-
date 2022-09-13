457840fb7a69d779484c308aace69f60f7b4a230
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-13 18:22:53 +0530

    Time: 7 ms (69.59%), Space: 9 MB (67.16%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -2,28 +2,48 @@ using namespace std;
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        vector<char> s;
-        int n = chars.size();
-        int count = 1;
-        for(int i = 0; i< n; i++){
-            if(i+1<n && chars[i] == chars[i+1]){
-                count++;
-            }else{
-                s.push_back(chars[i]);
-                if(count>1){
-                    string str = to_string(count);
-                    cout<<str<<endl;
-                    for(auto ch: str){
-                        cout<<ch<<endl;
-                        s.push_back(ch);
-                    }
-                }
-                
-                count = 1;
-            }
-        }
-        chars = s;
-        return s.size();
+        // solution 1
+        // vector<char> s;
+        // int n = chars.size();
+        // int count = 1;
+        // for(int i = 0; i< n; i++){
+        //     if(i+1<n && chars[i] == chars[i+1]){
+        //         count++;
+        //     }else{
+        //         s.push_back(chars[i]);
+        //         if(count>1){
+        //             string str = to_string(count);
+        //             cout<<str<<endl;
+        //             for(auto ch: str){
+        //                 cout<<ch<<endl;
+        //                 s.push_back(ch);
+        //             }
+        //         }
+        //         count = 1;
+        //     }
+        // }
+        // chars = s;
+        // return s.size();
         
+        // solution 2
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
+        while(i<n){
+            int j = i+1;
+            while(j< n && chars[j] == chars[i]){
+                j++;
+            }
+            chars[ansIndex++] = chars[i];
+            int count = j - i;
+            if(count>1){
+                string cnt = to_string(count);
+                for(auto ch: cnt){
+                    chars[ansIndex++] = ch;
+                }
+            }
+            i = j ;
+        }
+        return ansIndex;
     }
 };
