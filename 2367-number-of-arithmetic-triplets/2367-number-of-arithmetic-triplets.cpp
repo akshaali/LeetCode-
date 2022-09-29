f5e7100e55692521e4f651c02ccfb62e0a465ec7
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-30 00:11:04 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (54.59%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,21 +1,15 @@
+using namespace std;
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        //  int ele[201];
-        // int ans = 0;
-        // for(auto n: nums){
-        //     if(n >= 2 * diff){
-        //         ans += (ele[n-diff] && ele[n-2*diff]);
-        //     }
-        //     ele[n] = true;
-        // }
-        // return ans;
-        int cnt[201] = {}, res = 0;
-    for (auto n : nums) {
-        if (n >= 2 * diff)
-            res += cnt[n - diff] && cnt[n - 2 * diff];
-        cnt[n] = true;
-    }
-    return res;
+         int ele[201] = {};
+        int ans = 0;
+        for(auto n: nums){
+            if(n >= 2 * diff){
+                ans += (ele[n-diff] && ele[n-2*diff]);
+            }
+            ele[n] = true;
+        }
+        return ans;
     }
 };
