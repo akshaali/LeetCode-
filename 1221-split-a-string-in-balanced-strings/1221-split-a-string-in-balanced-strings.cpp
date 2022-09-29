0d218c99da22e7a3da504cdb456d4c9335252b94
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-09-29 13:04:34 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (38.72%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans = 0;
+        int val = 0;
+        for(auto i: s){
+            if(i == 'L'){
+                val +=1;
+            }else{
+                val -=1;
+            }
+            if(val == 0) ans++;
+        }
+        return ans;
+        
+    }
+};
