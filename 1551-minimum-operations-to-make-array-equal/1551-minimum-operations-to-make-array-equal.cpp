2ac9e77c8e98ec4f5edbd02118d75b6e7ac63bb5
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-13 16:00:55 +0530

    Time: 3 ms (56.99%), Space: 6 MB (32.05%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        if(n & 1){ // Odd Case
+		int N = (n - 1) / 2;  // number of elements before mid element
+		return N * (N + 1); 
+        }
+        int N = n / 2;  // number of elements before mean of mid elements
+        return N * N; 
+    }
+};
