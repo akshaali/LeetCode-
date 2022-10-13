8678320ec70e91595c47e1c49e8b5c33d9def1a0
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-13 14:23:16 +0530

    Time: 74 ms (22.61%), Space: 20.5 MB (13.72%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int min1 = nums[0], min2 = nums[1];
-        int max1 = nums[nums.size() -1], max2 = nums[nums.size()-2];
-        return(max1*max2 - min1*min2);   
+        //O(nlog(n))
+        // sort(nums.begin(), nums.end());
+        // int min1 = nums[0], min2 = nums[1];
+        // int max1 = nums[nums.size() -1], max2 = nums[nums.size()-2];
+        // return(max1*max2 - min1*min2);   
+        //O(n)
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+        for(auto num : nums){
+            if(num>max1){
+                max2 = max1; //second greatest
+                max1 = num; //first greatest
+            }else if(num>max2){
+                max2 = num;
+            }
+            if(num<min1){
+                min2 = min1;
+                min1 = num;   
+            }else if(num<min2){
+                min2 = num;
+            }
+        }
+        return(max1*max2 - min1*min2);    
     }
 };
