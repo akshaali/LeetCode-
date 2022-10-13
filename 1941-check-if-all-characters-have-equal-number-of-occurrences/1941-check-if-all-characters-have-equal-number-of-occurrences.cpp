ae50c10bdeed5e6dba8d89defd52021626c6b5b4
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-13 13:41:36 +0530

    Time: 8 ms (51.23%), Space: 6.7 MB (66.02%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        unordered_map<char, int> freq;
+        for(auto ch:s){
+            freq[ch]++;
+        }
+        int occ = freq[s[0]];
+        for(auto oc: freq){
+            if(oc.second != occ) return false;
+        }
+        return true;
+    }
+};
