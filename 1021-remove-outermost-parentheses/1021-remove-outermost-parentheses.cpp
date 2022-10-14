f58af17a319cbf544321061354d7baac94b35d91
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-14 20:40:06 +0530

    Time: 12 ms (20.17%), Space: 6.5 MB (94.28%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,26 +2,35 @@ using namespace std;
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        vector<string> pri;
+        //extra space - O(n)
+        // vector<string> pri;
         string ans = "";
-        int leftP = 0, rightP = 0;
-        int j = 0;
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] == '('){
-                leftP++;
-            }else if(s[i] == ')'){
-                rightP++;
-            }
-            if(leftP == rightP){
-                cout<<s.substr(j,i-j+1)<<endl;
-                pri.push_back(s.substr(j,i-j+1));
-                j = i +1;
-            }
+        // int leftP = 0, rightP = 0;
+        // int j = 0;
+        // for(int i = 0; i<s.size(); i++){
+        //     if(s[i] == '('){
+        //         leftP++;
+        //     }else if(s[i] == ')'){
+        //         rightP++;
+        //     }
+        //     if(leftP == rightP){
+        //         cout<<s.substr(j,i-j+1)<<endl;
+        //         pri.push_back(s.substr(j,i-j+1));
+        //         j = i +1;
+        //     }
+        // }
+        // for(auto i: pri){
+        //     cout<<i<<endl;
+        //     ans += i.substr(1,i.size()-2);
+        // }
+        //Try with space O(1)
+        int open = 0;
+        for(auto brac: s){
+            if(brac == '(' && open++>0) ans += brac;
+            else if(brac == ')' && open-->1) ans += brac;
+            // cout<<ans<<endl;
         }
-        for(auto i: pri){
-            cout<<i<<endl;
-            ans += i.substr(1,i.size()-2);
-        }
+        
         return ans;
     }
 };
