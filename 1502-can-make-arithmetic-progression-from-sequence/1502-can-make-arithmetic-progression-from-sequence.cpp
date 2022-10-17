8f7f00d5db17bc6a38a3ed5718b9cd975bef629e
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-17 17:49:43 +0530

    Time: 0 ms (100.00%), Space: 9 MB (92.73%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,15 +1,45 @@
+using namespace std;
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int n = arr.size();
-        if(n==1) return true;
-        int diff = (arr[n-1] - arr[0]) /(n-1);
-        for(int i = 1; i<n; i++){
-            if((arr[i] -arr[i-1]) != diff){
-                return false;
+        // // sort(arr.begin(), arr.end());
+        // int n = arr.size();
+        // if(n==1) return true;
+        // int min = INT_MAX, max = INT_MIN;
+        // for(auto a : arr){
+        //     if(a>max) max = a;
+        //     if(a<min) min = a;
+        // }
+        // cout<<max<<" "<<min<<endl;
+        // int diff = (max-min) /(n-1);
+        // if ((max - min) % (arr.size() - 1)) return false;
+        // for(int i = 1; i<n; i++){
+        //     if((arr[i] -arr[i-1]) != diff){
+        //         return false;
+        //     }
+        // }
+        // return true;
+    
+        if (arr.size() <= 2) return true;
+        int min = INT_MAX, max = INT_MIN;
+        for (int num : arr) {
+            min = std::min(min, num);
+            max = std::max(max, num);
+        }
+        if ((max - min) % (arr.size() - 1) != 0) return false;
+        int d = (max - min) / (arr.size() - 1);
+
+        int i = 0;
+        while (i < arr.size()) {
+            if (arr[i] == min + i * d) i++;
+            else if ((arr[i] - min) % d != 0) return false;
+            else {
+                int pos = (arr[i] - min) / d;
+                if (pos < i || arr[pos] == arr[i]) return false;
+                std::swap(arr[i], arr[pos]);
             }
         }
         return true;
+    
     }
 };
