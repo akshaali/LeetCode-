c25b8ceace6d5428a95e8d1a8cb8b4ff5103b197
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-23 01:26:20 +0530

    Time: 10 ms (64.15%), Space: 9.9 MB (93.35%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -2,23 +2,28 @@ using namespace std;
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max1 = 0, max2 = 0;
-        int i = -1, j = -1;
-        for(int k = 0; k<nums.size(); k++){
-            // cout<<max2<<" "<<max1<<endl;
-            if(nums[k]>max1) {
-                max2 = max1;
-                max1 = nums[k];
-                j = i;
-                i = k;
-            }
-            if(nums[k]>max2 && i != k){
-                max2 = nums[k];
-                j = k;
-            }
+        // int max1 = 0, max2 = 0;
+        // int i = -1, j = -1;
+        // for(int k = 0; k<nums.size(); k++){
+        //     if(nums[k]>max1) {
+        //         max2 = max1;
+        //         max1 = nums[k];
+        //         j = i;
+        //         i = k;
+        //     }
+        //     if(nums[k]>max2 && i != k){
+        //         max2 = nums[k];
+        //         j = k;
+        //     }
+        // }
+        // return (nums[i] - 1)*(nums[j] - 1);
+        auto m1 = 0, m2 = 0;
+        for (auto n: nums) {
+            if (n > m1)
+                m2 = exchange(m1, n);
+            else
+                m2 = max(m2, n);
         }
-        // cout<<i<<" "<<j<<endl;
-        // return 0;
-        return (nums[i] - 1)*(nums[j] - 1);
+        return (m1 - 1) * (m2 - 1);
     }
 };
