196f04863add3ced2281f41d8b5724d2af053af0
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-22 17:12:07 +0530

    Time: 322 ms (70.05%), Space: 66.2 MB (53.52%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -2,27 +2,17 @@ using namespace std;
 class Solution {
 public:
     long long minimalKSum(vector<int>& nums, int k) {
-        // long long sum = 0;
-        // int curr = 1;
-        // sort(nums.begin(), nums.end());
-        // for(int i = 0; i<nums.size() && k>0; i++){
-        //     int take = max(0, min(nums[i] - curr, k));
-        //     sum += (curr + take) * (curr + take - 1) / 2 - curr * (curr - 1) / 2;
-        //     k -= take;
-        //     curr = nums[i] + 1;
-        //     // cout<<take<<" "<<k<<" "<<curr<<" "<<sum<<endl;
-        // }
-        // return sum;
-        long long res = 0, cur = 1;
-    nums.push_back(INT_MAX);
-    sort(begin(nums), end(nums));
-    for (int i = 0; k > 0 && i < nums.size(); ++i) {
-        int take = max(0, min(k, nums[i] - (int)cur));
-        res += (cur + take) * (cur + take - 1) / 2 - cur * (cur - 1) / 2;
-        k -= take;
-        cur = (long long)nums[i] + 1;
-    }
-    return res;
+        long long sum = 0, curr = 1;;
+        nums.push_back(INT_MAX);
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i<nums.size() && k>0; i++){
+            int take = max(0, min(nums[i] - (int)curr, k));
+            sum += (curr + take) * (curr + take - 1) / 2 - curr * (curr - 1) / 2;
+            k -= take;
+            curr = (long long)nums[i] + 1;
+            // cout<<take<<" "<<k<<" "<<curr<<" "<<sum<<endl;
+        }
+        return sum;
     }
     
 };
