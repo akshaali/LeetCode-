9dbae172886e859f9393cd2abfa25dfcb9b32f03
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-10-27 16:26:05 +0530

    Time: 286 ms (20.80%), Space: 218.1 MB (11.60%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int ans = INT_MIN;
-        for(auto n: nums){
-            pq.push(n);
-            if(pq.size()>k) pq.pop();
-        }
-        return pq.top();
+    //priority ques
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int, vector<int>, greater<int>> pq;
+//         int ans = INT_MIN;
+//         for(auto n: nums){
+//             pq.push(n);
+//             if(pq.size()>k) pq.pop();
+//         }
+//         return pq.top();
+//     }
+    public:
+    int findKthLargest(vector<int>nums, int k) {
+      return quickSelect(nums, 0, nums.size() - 1, k);
     }
+
+int quickSelect(vector<int> nums, int low, int high, int k) {
+  int pivot = low;
+
+  // use quick sort's idea
+  // put nums that are <= pivot to the left
+  // put nums that are  > pivot to the right
+  for (int j = low; j < high; j++) {
+    if (nums[j] <= nums[high]) {
+      swap(nums[pivot++], nums[j]);
+    }
+  }
+  swap(nums[pivot], nums[high]);
+  
+  // count the nums that are > pivot from high
+  int count = high - pivot + 1;
+  // pivot is the one!
+  if (count == k) return nums[pivot];
+  // pivot is too small, so it must be on the right
+  if (count > k) return quickSelect(nums, pivot + 1, high, k);
+  // pivot is too big, so it must be on the left
+  return quickSelect(nums, low, pivot - 1, k - count);
+}
 };
+
+
