f43dd3fc88da4616c7c37bce8ad59ccb7ad23eef
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2022-11-01 17:36:36 +0530

    Time: 828 ms (61.10%), Space: 286.5 MB (89.23%) - LeetHub

---
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+class Solution {
+public:
+    vector<int> maxScoreIndices(vector<int>& nums) {
+        int score = accumulate(begin(nums), end(nums), 0), max_score = score;
+        // cout<<max_score<<" "<<score<<endl;
+        vector<int> res{0};
+        for (int i = 0; i < nums.size(); ++i) {
+            score += (nums[i] == 0) - nums[i];
+            if (score > max_score) {
+                max_score = score;
+                res.clear();
+            }
+            if (score == max_score)
+                res.push_back(i + 1);
+        }
+        return res;
+    }
+};
