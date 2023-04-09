426e27a234f3ad16ee8053fdf4e2f93788164595
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-04-09 12:08:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.79%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,14 @@
+using namespace std;
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while(b != 0){
+            
+            int temp = (unsigned int)(a&b)<<1;
+            cout<<temp<<endl;
+            a = a^b;
+            b = temp;
+        }
+        return a;
     }
 };
