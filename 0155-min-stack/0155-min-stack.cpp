6a18da94be620833dafd120cea11aa144257362f
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-04-17 10:40:41 +0530

    Time: 23 ms (65.68%), Space: 16.4 MB (64.59%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,38 +1,58 @@
+using namespace std;
 class MinStack {
 public:
-    vector<int> arr;
-    int peek=-1;
-    int size=0;
+    vector<long int> arr;
+    int size = 0;
+    long int mini = INT_MAX;
     MinStack() {
-        // int *arr;
-        // int top=-1;
-        // int size=0;
+    }
+    void print(){
+        for(int i = 0; i<arr.size(); i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
     }
     
     void push(int val) {
-        arr.push_back(val);
-        peek++;
+        if(size == 0){
+            arr.push_back(val);
+            mini = val;
+        }else{
+            long int data = val-mini;
+            arr.push_back(data);
+            if(val<mini) mini = val;
+        }
         size++;
     }
     
     void pop() {
-        arr.pop_back();
+        long int top = arr[size-1];
+        if(top<0){
+           long int oldMini = mini - top;
+            mini = oldMini;
+             arr.pop_back();
+        }else{
+             arr.pop_back();
+        }
         size--;
-        peek--;
     }
     
     int top() {
-        return arr[peek];
+        long int top_ = arr[size-1];
+        // cout<<"top_ "<<top_<<" mini "<<mini<<endl;
+        if(top_< 0){
+           return mini;
+        }else{
+            if(size == 1) return top_;
+            long int data = int(top_ + mini);
+            // print();
+            // cout<<"data "<<data<<endl;
+             return data;
+        }
     }
     
     int getMin() {
-        int min_ = INT_MAX;
-        for(int i=0; i<size; i++){
-            if(arr[i]<min_){
-                min_=arr[i];
-            }
-        }
-        return min_;
+        return (long)mini;
     }
 };
 
