375f0c9219de589f10bf424716075bd5439db6d9
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-05-07 09:10:29 +0530

    Time: 112 ms (10.88%), Space: 69.6 MB (9.47%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int size = candidates.size();
+        vector<vector<int>> output;
+        vector<int> subset;
+        targetSum(0, 0,output, subset, size, candidates, target);
+        return output;
+        
+    }
+    void targetSum(int index, int sum, vector<vector<int>> &output, vector<int> subset, int size, vector<int> &candidates, int target){
+        if(index == size || sum >=target){
+            if(sum == target){
+                output.push_back(subset);
+            }
+            return;
+        }
+        sum += candidates[index];
+        subset.push_back(candidates[index]);
+        targetSum(index, sum, output, subset, size,candidates, target);
+        sum -= candidates[index];
+        subset.pop_back();
+        targetSum(index+1, sum, output, subset, size,candidates, target);
+    }
+    
+};
