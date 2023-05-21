7d2d4a237a17ae56fb1490c035c183f90ecc167e
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-05-21 12:03:42 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (96.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,19 @@
-//bottom -Up DP
+//bottom -Up DP() - space optimization
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = nums[0];
+        // vector<int>dp(n+1, -1);
+        // dp[0] = 0;
+        // dp[1] = nums[0];
+        int prev1 = 0;
+        int prev2 = nums[0];
         
         for(int i = 2; i<=n; i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+            int curr = max(prev2, nums[i-1] + prev1);
+            prev1 = prev2;
+            prev2 = curr;
         }
-        return dp[n];
+        return prev2;
     }
 };
