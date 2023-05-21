765792f3a4af06784717bae65d5655070202be84
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-05-21 12:00:34 +0530

    Time: 3 ms (35.70%), Space: 7.8 MB (29.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,15 @@
-//top-down - memoization
+//bottom -Up DP
 class Solution {
 public:
-    int solve(int index, vector<int>& nums, vector<int> &dp){
-        if(index == 0) return nums[0];
-        if(index<0) return 0;
-        if(dp[index] != -1) return dp[index];
-        int pick = solve(index-2, nums, dp) + nums[index];
-        int notPick = solve(index-1, nums, dp);
-        int ans = max(pick, notPick);
-        dp[index] = ans;
-        return ans;
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n+1, -1);
-        return solve(n-1, nums, dp);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i = 2; i<=n; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        return dp[n];
     }
 };
