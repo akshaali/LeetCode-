674d77372f5dc83126faa39555866eaa339a8b1e
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-05-26 15:39:29 +0530

    Time: 8 ms (48.92%), Space: 9.3 MB (5.45%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,43 @@
+using namespace std;
 class Solution {
 public:
-        int solve(int row, int col, vector<vector<int>>& triangle,vector<vector<int>>& dp){
-                if(row>=triangle.size() || col >=triangle[row].size()) return INT_MAX;
-                if(row == triangle.size()-1) return triangle[row][col];
-                if(dp[row][col] != -1) return dp[row][col];
-                int down = triangle[row][col] + solve(row+1, col, triangle, dp);
-                int diagonal = triangle[row][col] + solve(row+1, col+1, triangle, dp);
-                int ans = min(down, diagonal);
-                dp[row][col] = ans;
-                return ans;
-                
+        void print(vector<int> arr){
+                cout<<"[ ";
+                for(auto a: arr){
+                        cout<<a<<", ";
+                }
+                cout<<"]"<<endl;
+        }
+        int minimum(vector<int> arr){
+                int mini = INT_MAX;
+                for(auto a: arr){
+                        mini = min(mini, a);
+                }
+                return mini;
         }
     int minimumTotal(vector<vector<int>>& triangle) {
             int n = triangle.size();
             vector<vector<int>>dp(n, vector<int>(n,-1));
-        int ans = solve(0,0,triangle, dp);
+            vector<int>prev(n,0);
+            for(int i = 0; i<n; i++){
+                    vector<int>curr(n,0);
+                    for(int j = 0; j<triangle[i].size(); j++){
+                            if(i == 0 && j == 0){
+                                    curr[j] = triangle[i][j];
+                                    continue;
+                            }
+                            int diagonal = INT_MAX, down = INT_MAX;
+                            if(i>0 && j<triangle[i-1].size()){
+                                   down =  triangle[i][j] + prev[j];
+                            }
+                            if(i>0 && j >0 && j<=triangle[i-1].size()){
+                                    diagonal = triangle[i][j] + prev[j-1];
+                            }
+                            curr[j] = min(diagonal, down);
+                    }
+                    prev = curr;
+            }
+            int ans = minimum(prev);
             return ans;
     }
 };
