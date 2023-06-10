7ab423a1862e6a6b1fd4e8325fa1b134e481c282
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-06-11 01:14:53 +0530

    Time: 718 ms (11.05%), Space: 153.6 MB (7.37%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(int num, int n, vector<vector<int>> &dp){
-        if(num==1){
-           return n/num;
-        }
-        if(dp[num][n] != -1) return dp[num][n];
-        int notTake = solve(num-1, n,dp);
-        int take = 1e9;
-        if(n-num*num>=0){
-            take = 1 + solve(num, n-num*num,dp);
-        }
-        int ans = min(take, notTake);
-        dp[num][n] = ans;
-        return ans;
-    }
+    
     int numSquares(int n) {
         int num = sqrt(n); 
-        vector<vector<int>>dp(num+1, vector(n+1, -1));
-        return solve(num,n,dp);
+        vector<vector<int>>dp(num+1, vector(n+1, 0));
+        for(int i = 0; i<=n; i++){
+            dp[1][i] = i;
+        }
+        for(int i =2; i<=num; i++){
+            for(int j = 0; j<=n; j++){
+                int notTake = dp[i-1][j];
+                int take = 1e9;
+                if(j-i*i>=0){
+                    take = 1 + dp[i][j-i*i];
+                }
+                dp[i][j] = min(take, notTake);
+            }
+        }
+        return dp[num][n];
     }
 };
