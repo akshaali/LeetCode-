52acf8c17f1804998aab8ac46f987a2aaa6ab08d
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-07-09 23:52:12 +0530

    Time: 1058 ms (5.01%), Space: 283.8 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -7,7 +7,7 @@ public:
         if(dp[index][trans][buy] != -1) return dp[index][trans][buy];
         int _profit1 = INT_MIN, _profit2 = INT_MIN;
         if(buy){
-            int profit1 = -1*prices[index] + solve(index+1, prices, trans-1, 0,dp);
+            int profit1 = -1*prices[index] + solve(index+1, prices, trans, 0,dp);
             int profit2 = 0 + solve(index+1, prices, trans, buy,dp);
             _profit1 =  max(profit1,profit2);
         }else{
@@ -21,7 +21,7 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         n = prices.size();
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(5,vector<int>(2,-1)));
-        return solve(0,prices,4,1,dp);
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(3,vector<int>(2,-1)));
+        return solve(0,prices,2,1,dp);
     }
 };
