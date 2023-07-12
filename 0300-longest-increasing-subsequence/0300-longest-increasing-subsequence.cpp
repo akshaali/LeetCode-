019a8f3e03964a007059722e1819505943d3afcb
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-07-12 09:47:20 +0530

    Time: 520 ms (40.73%), Space: 292.4 MB (8.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,22 @@
 // Tabulation
 class Solution {
 public:
-    int n;
-    int solve(int index, int prevIndex, vector<int>& nums, vector<vector<int>>& dp){
-        if(index == n) return 0;
-        if(dp[index][prevIndex+1] != -1) return dp[index][prevIndex+1];
-        int take = INT_MIN;
-        int notTake = 0 + solve(index+1, prevIndex, nums, dp);
-        if(prevIndex == -1 || nums[prevIndex] < nums[index]){
-            take = 1 + solve(index+1, index, nums, dp);
-        }
-        int ans = max(take, notTake);
-        dp[index][prevIndex+1] = ans;
-        return ans;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
+        int n = nums.size();
         vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        vector<int>after(n+1, 0), curr(n+1, 0);
         for(int index = n-1; index>=0; index--){
             for(int prevIndex = index-1; prevIndex>=-1; prevIndex--){
                 int take = INT_MIN;
-                int notTake = 0 + dp[index+1][prevIndex+1];
+                int notTake = 0 + after[prevIndex+1];
                 if(prevIndex == -1 || nums[prevIndex] < nums[index]){
-                    take = 1 + dp[index+1][index+1];
+                    take = 1 + after[index+1];
                 }
                 int ans = max(take, notTake);
-                dp[index][prevIndex+1] = ans;
+                curr[prevIndex+1] = ans;
             }
+            after = curr;
         }
-        return dp[0][0];
+        return after[0];
     }
 };
