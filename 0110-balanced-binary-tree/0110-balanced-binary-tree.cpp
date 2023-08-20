564bf87b9647482ae1df5a3f820fc516d9f223b7
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-08-20 21:05:23 +0530

    Time: 9 ms (74.57%), Space: 22 MB (7.14%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -19,15 +19,37 @@ public:
         int ans = max(left, right) + 1;
         return ans;
     }
+    pair<bool, int> isBalancedFast(TreeNode* root){
+        if(root == NULL){
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        pair<bool, int> left = isBalancedFast(root->left);
+        pair<bool, int> right = isBalancedFast(root->right);
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        int heightDiff = abs(left.second - right.second)<=1;
+        pair<bool, int> ans;
+        if(leftAns && rightAns && heightDiff) {
+            ans.first = true;
+        }else{
+            ans.first = false;
+        }
+        ans.second = max(left.second, right.second) + 1;
+        return ans;
+        
+    }
     bool isBalanced(TreeNode* root) {
-        if(root == NULL) return true;
+//         if(root == NULL) return true;
         
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+//         bool left = isBalanced(root->left);
+//         bool right = isBalanced(root->right);
         
-        bool heightDiff = abs(height(root->left) - height(root->right)) <= 1;
+//         bool heightDiff = abs(height(root->left) - height(root->right)) <= 1;
         
-        if(left && right && heightDiff) return true;
-        return false;
+//         if(left && right && heightDiff) return true;
+//         return false;
+        return isBalancedFast(root).first;
     }
 };
