f6350c6605411393eff238186ffa046565996871
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-10-03 23:29:29 +0530

    Time: 5 ms (90.87%), Space: 16.1 MB (33.90%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> majority;
+        int maj1=INT_MIN, maj2=INT_MIN, count1=0, count2=0;
+        for(auto ele: nums){
+            if(maj1 == ele) count1++;
+            else if(maj2 == ele) count2++;
+            else if(count1 == 0){ 
+                maj1 = ele;
+                count1++;
+            }
+            else if(count2 == 0){
+                 maj2 = ele;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        int maj = nums.size()/3;
+        count1 = 0, count2 = 0;
+        cout<<maj1<<" "<<maj2<<endl;
+        for(auto ele: nums){
+            if(ele == maj1) count1++;
+            if(ele == maj2) count2++;
+        }
+        if(count1>maj) majority.push_back(maj1);
+        if(count2>maj) majority.push_back(maj2);
+        return majority;
+        
+    }
+};
