913a764d124c83b516133858ae1ecbf62b77c330
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2023-10-17 09:46:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,22 +2,42 @@ using namespace std;
 class Solution {
 public:
     double myPow(double x, int n) {
-        double num = 1;
-        long long nn = n;
-        if(nn < 0) nn = -nn;
-        while(nn>0){
-            if(nn%2==1){
-                num = num * x;
-                nn--;
-            }
-            else{
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        double ans = 1;
+        long long m = n;
+        if(m<0) m = -m;
+        cout<<n<<" "<<m<<endl;
+        while(m>0){
+            if(m%2 == 1){
+                ans = ans*x;
+                m -= 1;
+            }else{
                 x = x*x;
-                nn/=2;
+                m /= 2;
             }
         }
-        if(n < 0){
-            num = 1.0/num;
-        }
-        return num;
+        if(n<0) return 1.0/ans;
+        return ans;
+       
+       
+        
+        // double num = 1;
+        // long long nn = n;
+        // if(nn < 0) nn = -nn;
+        // while(nn>0){
+        //     if(nn%2==1){
+        //         num = num * x;
+        //         nn--;
+        //     }
+        //     else{
+        //         x = x*x;
+        //         nn/=2;
+        //     }
+        // }
+        // if(n < 0){
+        //     num = 1.0/num;
+        // }
+        // return num;
     }
 };
