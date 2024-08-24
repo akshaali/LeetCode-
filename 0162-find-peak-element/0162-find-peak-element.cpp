e051facffa41cc4f37deb43373362901e32cdd63
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2024-08-24 23:50:21 +0530

    Time: 3 ms (64.63%), Space: 11.5 MB (62.37%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,12 +8,14 @@ public:
         int start = 1, end = n-2;
         while(start<=end){
             int mid = end - (end-start)/2;
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
-            else if(nums[mid]>nums[mid-1]){
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(nums[mid+1]>nums[mid]){
                 start = mid+1;
             }else{
                 end = mid-1;
             }
+            
         }
         return -1;
     }
