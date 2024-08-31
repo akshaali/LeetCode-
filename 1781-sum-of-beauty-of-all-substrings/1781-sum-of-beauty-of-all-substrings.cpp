9aad2429c1d1361378df5940128c30a82566356b
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2024-09-01 00:00:29 +0530

    Time: 68 ms (93.70%), Space: 10.1 MB (84.73%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -5,17 +5,17 @@ public:
         int n = s.size();
         for(int i = 0; i<n; i++){
             vector<int>cnt(26, 0);
-            int maxF = 0;
-            int minF = 0;
+            int maxF = INT_MIN;
             for(int j = i; j<n; j++){
+                int minF = INT_MAX;
                 int indx = s[j] - 'a';
                 maxF = max(maxF, ++cnt[indx]);
-                if(minF>=cnt[indx]-1){
-                    minF = cnt[indx];
+                // if(minF>=cnt[indx]-1){
+                    // minF = cnt[indx];
                     for(int k = 0; k<26; k++){
                         minF = min(minF, cnt[k] == 0 ? INT_MAX : cnt[k]);
                     }
-                }
+                // }
                 // cout<<"string is: "<<s.substr(i, j-i+1)<<" maxF: "<<maxF<<" minF: "<<minF<<endl;
                 ans += (maxF - minF);
             }
