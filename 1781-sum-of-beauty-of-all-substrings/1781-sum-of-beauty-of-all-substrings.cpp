2e6997b78a3d8ee7bf997a37ff20c1b5ed124973
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2024-08-31 23:55:55 +0530

    Time: 33 ms (98.47%), Space: 10.2 MB (81.52%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans = 0;
+        int n = s.size();
+        for(int i = 0; i<n; i++){
+            vector<int>cnt(26, 0);
+            int maxF = 0;
+            int minF = 0;
+            for(int j = i; j<n; j++){
+                int indx = s[j] - 'a';
+                maxF = max(maxF, ++cnt[indx]);
+                if(minF>=cnt[indx]-1){
+                    minF = cnt[indx];
+                    for(int k = 0; k<26; k++){
+                        minF = min(minF, cnt[k] == 0 ? INT_MAX : cnt[k]);
+                    }
+                }
+                // cout<<"string is: "<<s.substr(i, j-i+1)<<" maxF: "<<maxF<<" minF: "<<minF<<endl;
+                ans += (maxF - minF);
+            }
+        }
+        return ans;
+    }
+};
