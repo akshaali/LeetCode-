d11fd286b5070cb74532450911ae0f3e79de9b19
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2024-10-09 01:36:19 +0530

    Time: 7 ms (97.62%), Space: 20.4 MB (58.78%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int>candies(n,1);
+        for(int i = 1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        // int forward = 0;
+        // for(auto candy: candies){
+        //     cout<<candy<<" ";
+        //     forward += candy;
+        // }
+        // cout<<"forward: "<<forward<<endl;
+        for(int i = n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1]+1);
+            }
+        }
+        int ans = 0;
+        for(auto candy: candies){
+            ans += candy;
+        }
+        return ans;
+    }
+};
