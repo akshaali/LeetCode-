9dfc6c70fe1ad86ac78aa1929f349d87e07a3c50
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2024-11-26 01:15:09 +0530

    Time: 1012 ms (31.20%), Space: 11.8 MB (51.92%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int m,n;
-    string word;
     int len;
-    bool solve(vector<vector<char>>& board, int row, int col,int index){
+    bool solve(vector<vector<char>>& board, string word, int row, int col,int index){
         if(index >= len) return true;
         if(row<0 || row>=m) return false;
         if(col<0 || col>=n) return false;
         if(board[row][col] != word[index]) return false;
-        // if(isVisited[row][col] == 1) return false;
-        // int ind = index;
-        // if(board[row][col] == word[index]){
-        //     ind++;
-        // }
         char val =  board[row][col];
         board[row][col] = '*';
-        bool down = solve(board, row+1, col, index+1);
-        bool up = solve(board, row-1, col, index+1);
-        bool left = solve(board, row, col-1, index+1);  
-        bool right = solve(board, row, col+1, index+1);
+        bool down = solve(board,word, row+1, col, index+1);
+        bool up = solve(board,word, row-1, col, index+1);
+        bool left = solve(board,word, row, col-1, index+1);  
+        bool right = solve(board,word, row, col+1, index+1);
         bool ans = down || up || right || left;
         board[row][col] = val;
         return ans;
     }
-    bool exist(vector<vector<char>>& board, string _word) {
+    bool exist(vector<vector<char>>& board, string word) {
         m = board.size();
         n = board[0].size();
-        word = _word;
-        len = _word.size();
-        vector<vector<int>> isVisited(m, vector<int>(n, 0));
+        len = word.size();
         for(int i = 0; i < m; i++) 
             for(int j = 0; j < n; j++) 
-                if(board[i][j] == word[0] && solve(board, i, j, 0)) return true;
+                if(board[i][j] == word[0] && solve(board, word, i, j, 0)) return true;
         return false;
     }
 };
