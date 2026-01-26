04a18116441b7c8fb91d212436fc910ef3ee846b
Aksha <41459801+akshaali@users.noreply.github.com>
2026-01-26 20:01:19 +0530

    Time: 1 ms (46.4%), Space: 9.7 MB (93.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,35 +3,15 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int start = 0, end = s.size()-1;
-        cout<<isalnum('0')<<endl;
         while(start<end) {
-            if (!isalnum(s[start])){ 
+            if(!isalnum(s[start])){
                 start++;
-             }
-		    if (!isalnum(s[end])) end--;
+            }
+		    if(!isalnum(s[end])) end--;
             if(isalnum(s[start]) && isalnum(s[end])) {
                 if (tolower(s[start++])!=tolower(s[end--])) return false;
             }
 	    }
-        // while(start<end){
-        //     //  if(tolower(s[start])>= 'a' && tolower(s[start]) <= 'z' && tolower(s[end])>='a' && tolower(s[end]) <= 'z' ){
-        //     //     if(tolower(s[start++]) != tolower(s[end--])){
-        //     //         return false;
-        //     //     }
-        //     // }
-        //      if(!(tolower(s[end])>='a' && tolower(s[end]) <= 'z')){
-        //         end--;
-        //     }
-        //     else if(!(tolower(s[start])>= 'a' && tolower(s[start]) <= 'z')){
-        //         cout<<"Value: "<<s[start]<<" "<<s[end];
-        //         start++;
-        //         cout<<"Value: "<<s[start]<<" "<<s[end];
-        //     }else{
-        //         if(tolower(s[start++]) != tolower(s[end--])){
-        //             return false;
-        //         }
-        //     }
-        // }
         return true;
     }
 };
