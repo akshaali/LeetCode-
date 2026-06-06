ef4497babc2bcd327476e8e01b2f3874036b0e60
Aksha <41459801+akshaali@users.noreply.github.com>
2026-06-06 13:56:48 +0530

    Time: 0 ms (100%), Space: 9.5 MB (48.53%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,27 +1,21 @@
 class Solution {
+private:
+    static constexpr int MAX = 100001;
+    static inline int dp[MAX], pref[MAX];
+
+    static inline bool init = []() {
+        for (int i = 100; i < MAX; i++) {
+            int r = i % 10;
+            int m = (i / 10) % 10;
+            int l = (i / 100) % 10;
+
+            bool isWave = (m > max(l, r)) | (m < min(l, r));
+            dp[i] = dp[i / 10] + isWave;
+            pref[i] = pref[i - 1] + dp[i];
+        }
+        return 0;
+    }();
+
 public:
-    int waviness(int x) {
-        string s = to_string(x);
-
-        int cnt = 0;
-
-        for (int i = 1; i < (int)s.size() - 1; i++) {
-            if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
-                (s[i] < s[i - 1] && s[i] < s[i + 1])) {
-                cnt++;
-            }
-        }
-
-        return cnt;
-    }
-
-    int totalWaviness(int num1, int num2) {
-        long long ans = 0;
-
-        for (int x = num1; x <= num2; x++) {
-            ans += waviness(x);
-        }
-
-        return ans;
-    }
+    int totalWaviness(int A, int B) { return pref[B] - pref[A - 1]; }
 };
