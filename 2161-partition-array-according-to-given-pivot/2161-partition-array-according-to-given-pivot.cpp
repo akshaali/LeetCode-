9d8a9fd5e3c408baab75b1b83935c0c33d1ca54c
Aksha <41459801+akshaali@users.noreply.github.com>
2026-06-08 22:32:45 +0530

    Time: 15 ms (11.39%), Space: 127.7 MB (88.18%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int low = 0, same = 0, high = 0;
-        for(auto i: nums){
-            if(i<pivot) low++;
-            else if(i == pivot) same++;
+        int n = nums.size();
+        int low = 0;
+        int same = 0;
+        int high = 0;
+        
+        for(int i = 0; i<n; i++){
+            if(nums[i]<pivot)low++;
+            else if(nums[i] == pivot) same++;
+            else high++;
         }
         high = low+same;
         same = low;
         low = 0;
-         vector<int> ans(nums.size());
-        for(auto i: nums){
-            if(i<pivot){
-                ans[low++] = i;
-            }else if(i == pivot){
-                ans[same++] = i;
-            }else{
-                ans[high++] = i;
+        vector<int>ans(n, -1);
+        for(int i =0; i<n; i++){
+            if(nums[i]<pivot){
+                ans[low] = nums[i];
+                low++;
+            }else if(nums[i] == pivot){
+                ans[same] = nums[i];
+                same++;
+            }
+            else{
+                ans[high] = nums[i];
+                high++;
             }
         }
         return ans;
-        
     }
 };
